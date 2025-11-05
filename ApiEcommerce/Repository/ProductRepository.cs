79d90e7a92172176346b3231951bd528639de9c0
Luis Gomez <gomezangelesluisalejandro@gmail.com>
2025-11-05 16:42:53 -0600

    feat: Implementar la interfaz IProductRepository y su clase ProductRepository con métodos para gestionar productos

---
+++ ApiEcommerce/Repository/ProductRepository.cs
@@ -0,0 +1,93 @@
+using System;
+using ApiEcommerce.Repository.IRepository;
+
+namespace ApiEcommerce.Repository;
+
+public class ProductRepository: IProductRepository
+{
+
+    private readonly ApplicationDbContext _db;
+
+    public ProductRepository(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    //Devuelve todos los productos en ICollection del tipo product
+    public ICollection<Product> GetProducts()
+    {
+        return _db.Products.OrderBy(p => p.Name).ToList();
+    }
+    //Devuelve todos los productos de una categoria especifica
+    public ICollection<Product> GetProductsInCategory(int categoryId)
+    {
+        return _db.Products.Where(p => p.CategoryId == categoryId).ToList();
+    }
+
+    //Busca un producto por su nombre
+    public ICollection<Product> SearchProduct(string name)
+    {
+        return _db.Products.Where(p => p.Name.ToLower().Trim() == name.ToLower().Trim()).ToList();
+    }
+
+    //Busca un producto por su id
+    public Product? GetProduct(int id)
+    {
+        return _db.Products.FirstOrDefault(p => p.Id == id);
+    }
+
+    public bool BuyProduct(string name, int quantity)
+    {
+        if(string.IsNullOrWhiteSpace(name) || quantity <= 0)
+        {
+            return false;
+        }
+        var product = _db.Products.FirstOrDefault(p => p.Name.ToLower().Trim() == name.ToLower().Trim());
+        if (product == null || product.Stock < quantity)
+        {
+            return false;
+        }
+        product.Stock -= quantity;
+        _db.Products.Update(product);
+        return Save();
+    }
+
+    //Verifica si un producto existe por su id
+    public bool ProductExists(int id)
+    {
+        return _db.Products.Any(c => c.Id == id);
+    }
+    //Verifica si un producto existe por su nombre
+    public bool ProductExists(string name)
+    {
+        return _db.Products.Any(c => c.Name.ToLower().Trim() == name.ToLower().Trim());
+    }
+    //Crea un nuevo producto
+    public bool CreateProduct(Product product)
+    {
+        product.CreationDate = DateTime.Now;
+        product.UpdateDate = DateTime.Now;
+        _db.Products.Add(product);
+        return Save();
+    }
+    //Actualiza un producto existente
+    public bool UpdateProduct(Product product)
+    {
+        product.CreationDate = DateTime.Now;
+        product.UpdateDate = DateTime.Now;
+        _db.Products.Update(product);
+        return Save();
+    }
+    //Elimina un producto
+    public bool DeleteProduct(Product product)
+    {
+        _db.Products.Remove(product);
+        return Save();
+    }
+    //Guarda los cambios en la base de datos
+    public bool Save()
+    {
+        return _db.SaveChanges() >= 0 ? true : false;
+    }
+
+
+}
