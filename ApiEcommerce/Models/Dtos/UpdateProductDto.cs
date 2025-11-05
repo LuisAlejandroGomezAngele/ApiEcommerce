16e4bef1718c03c0cbe9c1c5b52214d6bd418cc6
Luis Gomez <gomezangelesluisalejandro@gmail.com>
2025-11-05 16:16:36 -0600

    feat: Agregar DTOs para la creación y actualización de productos

---
+++ ApiEcommerce/Models/Dtos/UpdateProductDto.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace ApiEcommerce.Models.Dtos;
+
+public class UpdateProductDto {
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public decimal Price { get; set; }
+
+    public string ImgUrl { get; set; } = string.Empty;
+
+    public string SKU { get; set; } = string.Empty;
+
+    public int Stock { get; set; }
+
+    public DateTime? CreationDate { get; set; } = DateTime.Now;
+
+    public int CategoryId { get; set; }
+}
