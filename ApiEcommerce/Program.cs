a25486a8599836c1f9401e546b8e39dd75b9ea6f
Luis Gomez <gomezangelesluisalejandro@gmail.com>
2025-11-10 17:08:31 -0600

    feat: Agregar configuración de seguridad para Swagger con autenticación JWT

--- ApiEcommerce/Program.cs
+++ ApiEcommerce/Program.cs
@@ -6,6 +6,7 @@ using ApiEcommerce.Constants;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -45,7 +46,38 @@ builder.Services.AddAuthentication(options =>
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+  options =>
+  {
+      options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+      {
+          Description = "Nuestra API utiliza la Autenticación JWT usando el esquema Bearer. \n\r\n\r" +
+                      "Ingresa la palabra a continuación el token generado en login.\n\r\n\r" +
+                      "Ejemplo: \"12345abcdef\"",
+          Name = "Authorization",
+          In = ParameterLocation.Header,
+          Type = SecuritySchemeType.Http,
+          Scheme = "Bearer"
+      });
+      options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+      {
+        new OpenApiSecurityScheme
+        {
+          Reference = new OpenApiReference
+          {
+            Type = ReferenceType.SecurityScheme,
+            Id = "Bearer"
+          },
+          Scheme = "oauth2",
+          Name = "Bearer",
+          In = ParameterLocation.Header
+        },
+        new List<string>()
+      }
+    });
+  }
+);
 
 builder.Services.AddCors(options =>
 {
