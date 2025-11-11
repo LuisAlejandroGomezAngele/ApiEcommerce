e19e0e8d18a96eae67ccb35e8f507b12a00dca60
Luis Gomez <gomezangelesluisalejandro@gmail.com>
2025-11-11 17:45:12 -0600

    feat: Agregar soporte para Identity en ApplicationDbContext y crear migraciones correspondientes

--- ApiEcommerce/Data/ApplicationDbContext.cs
+++ ApiEcommerce/Data/ApplicationDbContext.cs
@@ -1,13 +1,22 @@
 using Microsoft.EntityFrameworkCore;
 using ApiEcommerce.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
     }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        // Configuraciones adicionales del modelo si es necesario
+    }
+
     public DbSet<Category> Categories { get; set; }
     public DbSet<Product> Products { get; set; }
     public DbSet<User> Users { get; set; }
+
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 }
